18fa8a41f64cd39568705babb0d130f9e8051887
iyeongjun <they6687@naver.com>
2018-06-24 14:43:54 +0900

    finish

---
+++ app/src/main/java/com/example/iyeongjun/gucknaesan/ui/activities/club/ClubDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.iyeongjun.gucknaesan.ui.activities.club
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.iyeongjun.gucknaesan.R
+
+class ClubDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_club_detail)
+    }
+}
